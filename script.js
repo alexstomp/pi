e519ef31ae6b0fe3e30b0e3ee6ecd62186cad49a
Alex Stomp <stompy1208@gmail.com>
2015-03-05 12:22:20 -0600

    adds responsive menu

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function() {
+
+    $('.js-menu-open').click(function() {
+        $('.mobile-nav').show();
+    });
+
+    $('.js-menu-close').click(function() {
+        $('.mobile-nav').hide();
+    });
+
+});
